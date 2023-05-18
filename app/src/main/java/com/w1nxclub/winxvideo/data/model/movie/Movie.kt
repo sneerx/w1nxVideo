80ab25c72a9fa874ea8a5e0542988568276e3755
enesarisoy <arisoyeness@gmail.com>
2023-05-18 16:38:48 +0300

    movie conflict

--- app/src/main/java/com/w1nxclub/winxvideo/data/model/movie/Movie.kt
+++ app/src/main/java/com/w1nxclub/winxvideo/data/model/movie/Movie.kt
@@ -3,6 +3,8 @@ package com.w1nxclub.winxvideo.data.model.movie
 import android.os.Parcelable
 import androidx.annotation.DrawableRes
 import androidx.room.Entity
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
 import com.w1nxclub.winxvideo.data.model.credits.Credits
 import com.w1nxclub.winxvideo.data.model.genre.Genre
 import kotlinx.parcelize.Parcelize
@@ -10,6 +12,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 @Entity(tableName = "movie")
 data class Movie(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String?,
     val overview: String?,
--- app/src/main/java/com/w1nxclub/winxvideo/data/model/tvshow/TvShow.kt
+++ app/src/main/java/com/w1nxclub/winxvideo/data/model/tvshow/TvShow.kt
@@ -3,6 +3,7 @@ package com.w1nxclub.winxvideo.data.model.tvshow
 import android.os.Parcelable
 import androidx.annotation.DrawableRes
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.w1nxclub.winxvideo.data.model.credits.Credits
 import com.w1nxclub.winxvideo.data.model.genre.Genre
 import kotlinx.parcelize.Parcelize
@@ -10,6 +11,7 @@ import kotlinx.parcelize.Parcelize
 @Entity(tableName = "tvshow")
 @Parcelize
 data class TvShow(
+    @PrimaryKey(autoGenerate = true)
     val id: Int?,
     val title: String?,
     val overview: String?,
